ee4eba87c9d9c4805d1a5943e0e2c92b7d184e54
Aisha Rashed <aisha1rashed@gmail.com>
2019-12-05 21:24:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Your code here
+const mapToNegativize = function(sourceArray){
+  let arr = sourceArray.map(e=>-e);
+  return arr;
+}
+const mapToNoChange = function(sourceArray){
+   let arr = sourceArray.map(e=>e);
+  return arr;
+}
+const mapToDouble = function(sourceArray){
+   let arr = sourceArray.map(e=>2*e);
+  return arr;
+}
+const mapToSquare = function(sourceArray){
+   let arr = sourceArray.map(e=>e*e);
+  return arr;
+}
+
+const reduceToTotal= function(sourceArray, startingPoint=0){
+  //return sourceArray.reduce(function(total , e){return e+total},startingPoint=0);
+  let result = startingPoint;
+  for(const i of sourceArray){
+    result += i;
+  }
+  return result;
+}
+const reduceToAllTrue= function(sourceArray){
+  let result= true;
+  sourceArray.forEach(e=>{
+    if(e===false){
+      result = false;
+    }
+  });
+  return result;
+}
+const reduceToAnyTrue= function(sourceArray){
+  let result= false;
+  sourceArray.forEach(e=>{
+    if(e===true){
+      result = true;
+    }
+  });
+  return result;
+}
