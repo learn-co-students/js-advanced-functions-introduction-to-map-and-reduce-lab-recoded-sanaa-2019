5350e7431b34c55477631ade4fe0d2b5fea03103
Sarah Alafif <sar1997yah@gmail.com>
2019-12-11 19:15:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+function mapToNegativize(Array){
+let arr = Array.map(function (Array) {
+  return Array *-1
+});
+return arr
+}
+function mapToNoChange(Array){
+  let arr = Array.map(function (Array) {
+  return Array
+});
+return arr
+}
+function mapToDouble(Array){
+  let arr = Array.map(function (Array) {
+  return Array*2
+});
+return arr
+}
+function mapToSquare(Array){
+    let arr = Array.map(function (Array) {
+  return Array*Array
+});
+return arr
+}
+function reduceToTotal(Array,accumulator=0){
+  let arr=Array.reduce(function (accumulator, currentValue) {
+  return accumulator + currentValue;
+}, accumulator);
+return arr;
+}
+function reduceToAllTrue(Array){
+  let arr=Array.every(x==true);
+return arr
+  
+}
